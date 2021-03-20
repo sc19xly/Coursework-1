33b0b1cb333d3bcc2aa474e7d3ac824b18882e2a
sc19xly <80677732+sc19xly@users.noreply.github.com>
2021-03-20 21:24:17 +0800

    third day
    improve the interface
    complete add book function
    complete the borrow book function

--- main.c
+++ main.c
@@ -12,32 +12,49 @@ void newuser();
 void newmanager();
 void readuserfile();
 void readmanagerfile();
+void readbookfile();
 int ifexist(char *name,int flag);
 void newuser();
 void newmanager();
 void userfunction(struct users*p);
+void managerinterface(struct manager*p);
+void borrowbook(struct users*b);
+//static char *ask_question(const char*question)
 
 struct users*head=NULL;
 struct users*end=NULL;
 struct manager*managerhead=NULL;
 struct manager*managerend=NULL;
-int usernum,managernum;
+//Book*bookhead=NULL;
+//Book*bookend=NULL;
+int usernum,managernum,booknum;
 
+char  *ask_question(const char *question) {
+
+	printf("%s",question);
+
+	const int size_step = 2;
+	char *answer = malloc(size_step);
+	answer[0] = 0;
+	char *next_chunk = answer;
+	int iteration = 0;
+	do {
+		answer = realloc(answer, size_step + iteration*size_step);
+		next_chunk = answer + strlen(answer); 
+		fgets(next_chunk, size_step, stdin);
+		next_chunk = answer + strlen(answer);
+		++iteration;
+	} while (* (next_chunk-1) != '\n');
+	*(next_chunk-1) = 0;
+	return answer;
+}
 void start()
 {
-	int choice;
-	int end=0;
-	while(1)
-	{
-		printf("\nWelcome to library management system\n");
-		printf("Select:\n");
-		printf("1.user log in.\n");
-		printf("2.manager log in.\n");
-		printf("3.new user.\n");
-		printf("4.new manager.\n");
-		printf("0.exit.\n");
-		printf("Your choice:\n");
-		scanf("%d",&choice);
+	int choice=5;
+	do {
+		char *answer=ask_question("\nPlease enter your choice:\n1)User login.\n2)Manager login.\n3)New user.\n4)New manager.\n5)Exit.\nYour choice:");
+		choice = atoi(answer);
+		free(answer);
 		switch(choice)
 		{
 			case 1:
@@ -52,15 +69,14 @@ void start()
 			case 4:
 				newmanager();
 				break;
-			case 0:
-				end=1;
+			case 5:
+				printf("See you next time!");
 				break;
 			default:
 				printf("Wrong!Please enter correct number.\n");
 		}
-		if(end==1)
-			break;
-	}
+	} while(choice!=5);
+	return;
 }
 void userlogin(){
 	struct users*p;
@@ -70,9 +86,9 @@ void userlogin(){
 	FILE*fp;
 	p=head;
 	printf("\nEnter your user name:\n");
-	scanf("%s",username);
+	fgets(username,100,stdin);
 	printf("\nEnter your password:\n");
-	scanf("%s",password);
+	fgets(password,100,stdin);
 	while(p!=NULL)
 	{
 		if(strcmp(p->name,username)==0)
@@ -81,7 +97,7 @@ void userlogin(){
 			while(strcmp(p->password,password)!=0&&num<3)
 			{
 				printf("\nWrong,enter your password again:\n");
-				scanf("%s",password);
+				fgets(password,100,stdin);
 				num++;
 			}
 			if(num==3)
@@ -93,16 +109,15 @@ void userlogin(){
 			{
 				printf("Succeed!Enter the UI.\n");
 				userfunction(p);
-				p=p->next;
 			}
-		}	
-		else if(exist==0)
+		}
+		p=p->next;
+	}	
+		if(exist==0)
 		{
 			printf("User do not exist, create first!\n");
 			printf("Back to the main interface.\n");
-			break;
 		}
-	}
 }
 void managerlogin(){
 	struct manager*p;
@@ -112,9 +127,9 @@ void managerlogin(){
 	FILE*fp;
 	p=managerhead;
 	printf("\nEnter your manager name:\n");
-	scanf("%s",managername);
+	fgets(managername,100,stdin);
 	printf("\nEnter your password:\n");
-	scanf("%s",password);
+	fgets(password,100,stdin);
 	while(p!=NULL)
 	{
 		if(strcmp(p->username,managername)==0)
@@ -123,7 +138,7 @@ void managerlogin(){
 			while(strcmp(p->username,managername)!=0)
 			{
 				printf("\nWrong password,enter your password again:\n");
-				scanf("%s",password);
+				fgets(password,100,stdin);
 				num++;
 			}
 			if(num==3)
@@ -134,16 +149,16 @@ void managerlogin(){
 			else
 			{
 				printf("Succeed!Enter the manager interface.\n");
-				printf("managerfunction\n");
-				p=p->next;
+				managerinterface(p);
 			}
 		}
-		else if(exist==0)
+		p=p->next;
+	}
+	 if(exist==0)
 		{
 			printf("Manager do not exist, create first!\n");
 			printf("Back to the main interface.\n");
 		}
-	}
 }
 void readuserfile(){
 	struct users*p;
@@ -174,6 +189,22 @@ void readmanagerfile(){
 			managerend->next=p;
 			managerend=p;
 			managerend->next=NULL;
+		}
+	}
+}
+void readbookfile(){
+	Book*p;
+	FILE*fp;
+	p=bookhead;
+	fp=fopen("book","ab+");
+	while(fread(p,sizeof(Book),1,fp))
+	{
+		if(p->next!=NULL)
+		{
+			p=(Book*)malloc(sizeof(Book));
+			bookend->next=p;
+			bookend=p;
+			bookend->next=NULL;
 		}
 	}
 }
@@ -211,7 +242,7 @@ void newuser(){
 	while(1)
 	{
 		printf("Enter username(length<20):\n");
-            	scanf("%s",username);
+            	fgets(username,100,stdin);
 		if(strlen(username)<20&&ifexist(username,0)!=1)
 			break;
 		else if(ifexist(username,0)==1)
@@ -223,7 +254,7 @@ void newuser(){
 	while(1)
 	{
 		printf("Enter user passsword(length<20):\n");
-		scanf("%s",password);
+		fgets(password,100,stdin);
 		if(strlen(password)<20)
 			break;
 		else
@@ -270,14 +301,15 @@ void newmanager(){
 	if(managernum!=0)
 	{
 		struct manager*b;
+		b=(struct manager*)malloc(sizeof(struct manager));
         	char managername[100];
         	char password1[100];
         	int exist=0,num=0;
         	b=managerhead;
         	printf("\nEnter a manager name:\n");
-        	scanf("%s",managername);
+        	fgets(managername,100,stdin);
         	printf("\nEnter the password:\n");
-        	scanf("%s",password);
+        	fgets(password1,100,stdin);
         	while(p!=NULL)
         	{
                 	if(strcmp(b->username,managername)==0)
@@ -286,7 +318,7 @@ void newmanager(){
                         	while(strcmp(b->username,managername)!=0)
                         	{
                                 	printf("\nWrong password,enter your password again:\n");
-                                	scanf("%s",password1);
+                                	fgets(password1,100,stdin);
                                 	num++;
                        	 	}
                         	if(num==3)
@@ -297,9 +329,9 @@ void newmanager(){
                         	else
                         	{
                                 	printf("Succeed!Creating a new manager\n");
-                                	b=b->next;
                         	}
                 	}
+			b=b->next;
 		}
         	if(exist==0)
         	{
@@ -310,7 +342,7 @@ void newmanager(){
 	while(1)
 	{
 		printf("Enter managername(lengthe<20):\n");
-		scanf("%s",name);
+		fgets(name,100,stdin);
 		if(strlen(name)<20&&ifexist(name,1)!=1)
 			break;
 		else if(ifexist(name,1)==1)
@@ -322,7 +354,7 @@ void newmanager(){
         while(1)
         {
                 printf("Enter manager passsword(length<20):\n");
-                scanf("%s",password);
+                fgets(password,100,stdin);
                 if(strlen(password)<20)
                         break;
                 else
@@ -359,66 +391,174 @@ void newmanager(){
         fclose(fp);
         printf("\nCreate successfully!");
         managernum++;
-}
+}	
 void userfunction(struct users*p){
-	int choice;
-	int end=0;
+	int choice=6;
 	char title[100];
 	char author[100];
 	char year[100];
-	while(1)
-	{
-		printf("\nUser Interface\n");
-		printf("1.Borrow books.\n");
-		printf("2.Return books.\n");
-		printf("3.Search a book by title.\n");
-		printf("4.Search a book by author.\n");
-		printf("5.Search a book by year.\n");
-		printf("6.List all books.\n");
-		printf("0.Return to main interface.");
-		printf("Give your choice:");
-		scanf("%i",&choice);
+	do{
+		char *answer= ask_question("\n User Interface\n\nPlease enter your choice:\n1)Borrow books.\n2)Return books.\n3)Search a book by title\n4)Search a book by author.\n5)Search a book by year.\n6)Back to the main interface.\nYour choice:");
+		choice=atoi(answer);
+		free(answer);
 		switch(choice)
 		{
 			case 1:
-				printf("borrowbook(p)\n");
+				borrowbook(p);
 				break;
 			case 2:
 				printf("returnbook(p)\n");
 				break;
 			case 3:
 				printf("Enter the title:\n");
-				scanf("%s",title);
+				fgets(title,100,stdin);
 				printf("find_book_by_title(title)\n");
 				break;
 			case 4:
 				printf("Enter the author:\n");
-				scanf("%s",author);
+				fgets(author,100,stdin);
 				printf("find_book_by_author(author)\n");
 				break;
 			case 5:
 				printf("Enter the year:\n");
-				scanf("%s",year);
+				fgets(year,100,stdin);
 				printf("find_book_by_year(year)\n");
 				break;
 			case 6:
-				printf("listbooks()\n");
-				break;
-			case 0:
-				end=1;
 				printf("Back to the main interface!\n");
 				break;
 			default:
 				printf("Wrong!Please enter correct number!\n");
 		}
-		if(end==1)
-			break;
-	}
+	}while(choice!=6);
+	return;
 }
-//Book inform{
-//	Book*head=NULL;
-//	Book*next=NULL;
-//};
+void Add(){
+	Book*p;
+	Book*b;
+	char a[100];
+	p=(Book*)malloc(sizeof(Book));
+	b=(Book*)malloc(sizeof(Book));
+	p=bookhead;
+	FILE*fp;
+	int exist=0;
+	if(booknum==0)
+	{
+		printf("Enter the book id:\n");
+	       	scanf("%d",&bookhead->id);
+		getcahr();
+        	printf("Enter the book title:\n");
+       		fgets(bookhead->bookname,100,stdin);
+        	printf("Enter the authors:\n");
+        	fgets(bookhead->author,100,stdin);
+        	printf("Enter the year of publication:\n");
+        	scanf("%d",&bookhead->year);
+        	printf("Enter the number of copies:\n");
+        	scanf("%d",&bookhead->copies);
+		bookend=bookhead;
+		bookend->next=NULL;
+	}
+	else{
+	printf("Enter the book id:\n");
+        scanf("%d",&b->id);
+	getchar();
+        printf("Enter the book title:\n");
+        fgets(b->bookname,100,stdin);
+        printf("Enter the authors:\n");
+        fgets(b->author,100,stdin);
+        printf("Enter the year of publication:\n");
+        scanf("%d",&b->year);
+        printf("Enter the number of copies:\n");
+        scanf("%d",&b->copies);
+	while(p!=NULL)
+	{
+		if(strcmp(p->bookname,b->bookname)==0)
+		{
+			exist=1;
+			p->copies=p->copies+b->copies;
+			p=p->next;
+		}
+	}
+	if(exist==0)
+	{
+		bookend->next=b;
+		bookend=b;
+		bookend->next=NULL;
+	}
+	}
+	booknum++;
+	p=bookhead;
+	fp=fopen("book","w");
+	while(p!=NULL)
+	{
+		fwrite(p,sizeof(Book),1,fp);
+		p=p->next;
+	}
+	fclose(fp);
+	printf("Succeed!Back to the manager interface!");
+}
+void managerinterface(struct manager*p){
+	int choice=3;
+	do{
+		char *answer=ask_question("\n Manager Interface\n\nPlease enter your choice:\n1)Add books.\n2)Remove books.\n3)Back to the main interface.\nYour choice: ");
+		choice=atoi(answer);
+		free(answer);
+		switch(choice)
+		{
+			case 1:
+				Add();
+				break;
+			case 2:
+				//Remove();
+				break;
+			case 3:
+				printf("Back to the main interface!\n");
+			default:
+				printf("Wrong!Please enter correct number!\n");
+		}
+	}while(choice!=3);
+	return;
+}
+void borrowbook(struct users*b){
+	Book*p;
+//	p->title=(char*)malloc(sizeof(char));
+	char bookname[100];
+	int exist=0;
+	p=bookhead;
+	while(p!=NULL)
+	{
+		printf("book name:%10s\nbookcount:%3d\n\n",p->title,p->copies);
+		p=p->next;
+	}
+	printf("Enter the book name you want to borrow:\n");
+	fgets(bookname,100,stdin);
+	p=bookhead;
+	printf("1\n");
+	while(p!=NULL)
+	{
+		if(strcmp(bookname,p->bookname)==0)
+		{	
+			if(p->copies==0)
+			{	printf("2\n");
+				printf("No books remaining.\n");
+			}
+		else
+			{
+				p->copies=p->copies-1;
+				b->booknum++;
+				strcpy(b->booklist[b->booknum-1],p->bookname);
+				exist=1;
+				break;
+			}
+		}
+		p=p->next;
+	}
+	if(exist==0)
+		printf("The book does not exist.\n");
+	else
+		printf("%s Borrow successfully!\n", p->title);
+	printf("Back to the user interface.\n");
+}
 
 int main()
 {	
@@ -426,8 +566,9 @@ int main()
 
 	head=(struct users *)malloc(sizeof(struct users));
 	managerhead=(struct manager*)malloc(sizeof(struct manager));
+	bookhead=(Book*)malloc(sizeof( Book));
 	
-	fp = fopen("user", "a+");
+	fp = fopen("user", "ab+");
 	usernum = fread(head, sizeof(struct users), 1, fp);
 	fclose(fp);
 	end = head;
@@ -438,6 +579,12 @@ int main()
 	fclose(fp);
 	managerend = managerhead;
 	readmanagerfile();
+
+	fp = fopen("book","a+");
+	booknum = fread(bookhead,sizeof(Book),1,fp);
+	fclose(fp);
+	bookend=bookhead;
+	readbookfile();
 	start();
 	return 0;
 }
