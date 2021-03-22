5a8abd4ba3b713ff3675733b0f121f7bcbc4248a
sc19xly <80677732+sc19xly@users.noreply.github.com>
2021-03-22 16:34:06 +0800

    complete version

---
+++ CMakeFiles/cw1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ddl/桌面/producer programming/cw1/main.c" "/home/ddl/桌面/producer programming/cw1/CMakeFiles/cw1.dir/main.c.o"
+  "/home/ddl/桌面/producer programming/cw1/manager.c" "/home/ddl/桌面/producer programming/cw1/CMakeFiles/cw1.dir/manager.c.o"
+  "/home/ddl/桌面/producer programming/cw1/user.c" "/home/ddl/桌面/producer programming/cw1/CMakeFiles/cw1.dir/user.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ CMakeFiles/cw1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cw1.dir/main.c.o"
+  "CMakeFiles/cw1.dir/manager.c.o"
+  "CMakeFiles/cw1.dir/user.c.o"
+  "cw1.pdb"
+  "cw1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
